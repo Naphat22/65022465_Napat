da555ec8b4f54781141e13d92d286a043c634c19
65022465 Naphat <119990657+Naphat22@users.noreply.github.com>
2023-08-17 11:36:43 +0700

    Update 02.java

--- 02.java
+++ 02.java
@@ -2,28 +2,28 @@ class Product {
     private String name;
     private double price;
 
-    // Constructor
+   
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    // Getter for name
+    
     public String getName() {
         return name;
     }
 
-    // Setter for name
+    
     public void setName(String name) {
         this.name = name;
     }
 
-    // Getter for price
+  
     public double getPrice() {
         return price;
     }
 
-    // Setter for price
+    
     public void setPrice(double price) {
         this.price = price;
     }
